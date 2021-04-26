6834b81f1a9330c4d04ab17a450e38d2ec3a50ff
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2021-04-26 04:20:43 -0400

    added home and registration components with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration/registration.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { HomeComponent } from './home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  {  path: 'registration', component: RegistrationComponent},
+  {  path: 'admin', component: AdminComponent},
+  {  path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { RegistrationComponent } from './registration/registration/registration.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
