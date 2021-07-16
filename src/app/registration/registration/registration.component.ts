ed380432865661bd6a1f1b302915a856ae874396
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2021-07-16 01:53:15 -0400

    added feature to hide the appropriate sections of the UI.

--- src/app/registration/registration/registration.component.ts
+++ src/app/registration/registration/registration.component.ts
@@ -7,13 +7,69 @@ import { DataService } from '../../data.service';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  public courseSelected: string = "empty";
+  public showPickADate: boolean = true;
+  public showStudentInfo: boolean = false;
+  public showSummary: boolean = false;
+  public showConfirmation: boolean = false;
+  courseSelected: string = "";
 
   constructor(private dataService: DataService) {
     this.courseSelected = dataService.getSelectedCourse();
    }
 
   ngOnInit(): void {
+  }
+
+  displayPickADateUI(): boolean {
+    if (this.showPickADate) {
+      return this.showPickADate;
+    }  else {
+      return false;
+    }
+  }
+
+  displayStudentInfoUI(): boolean {
+    if (this.showStudentInfo) {
+      return this.showStudentInfo;
+    } else {
+      return false;
+    }
+  }
+
+  displaySummaryUI(): boolean {
+    if (this.showSummary) {
+      return this.showSummary;
+    } else {
+      return false;
+    }
+  }
+
+  displayConfirmationUI(): boolean {
+    if (this.showConfirmation) {
+      return this.showConfirmation;
+    } else {
+      return false;
+    }
+  }
+
+  setShowPickADateToFalse() {
+    this.showPickADate = false;
+    this.showStudentInfo = true;
+  }
+
+  setShowStudentInfoToFalse() {
+    this.showStudentInfo = false;
+    this.showSummary = true;
+  }
+
+  setShowSummaryToFalse() {
+    this.showSummary = false;
+    this.showConfirmation = true;
+  }
+
+  setShowConfirmtationToFalse() {
+    this.showConfirmation = false;
+    this.showPickADate = true;
   }
 
 }
