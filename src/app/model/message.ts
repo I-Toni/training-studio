904ed1ee4da15098aa9f1f75781fd5325e0d5456
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2021-07-31 04:01:50 -0400

    added message model to the model folder

---
+++ src/app/model/message.ts
@@ -0,0 +1,11 @@
+export class Message {
+    
+    
+    constructor(public fname: string,
+                public lname: string,
+                public email: string,
+                public phone: string,
+                public message: string){}
+    
+
+  }
